26b8d9e1b0132d6e375840be1d51f4cbb3a9c861
JhaenCarloHe <jeancaarlo10@gmail.com>
2025-09-16 16:47:53 -0500

    Lógica principal

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Challenge Amigo Secreto
+
+// Lista de Amigos
+
+let listaAmigos = [];
+
+// Funciones de Apoyo
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""
+
+    for (let i = 0; i < listaAmigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = listaAmigos[i];
+        lista.appendChild(li);
+    }
+    return lista.length;
+}
+
+// Lógica del juego
+
+function agregarAmigo() {
+
+    let amigoIngresado = document.getElementById('amigo').value;
+    
+    if(amigoIngresado.trim() === "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+                listaAmigos.push(amigoIngresado);
+        limpiarCaja();
+        actualizarLista();
+        
+    }
+}
+
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("No hay amigos registrados para sortear.");
+        return;
+    }
+
+    let numeroAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSorteado = listaAmigos[numeroAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
